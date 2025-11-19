c94f7fbb829466901d19534101cebfeb50dd5f6d
LoganPal <logan.pallara@gmail.com>
2025-11-19 14:15:43 +0100

    fix: main conflict

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-<<<<<<< HEAD
-  await app.listen(3000);
-=======
   await app.listen(process.env.PORT ?? 3000);
->>>>>>> aa408d0d9169d9854428216203c4280d51feb9cf
 }
 bootstrap();
